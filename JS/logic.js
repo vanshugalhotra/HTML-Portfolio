e2dc349c41f13bd8cc6d030e1d789ac4e2948184
Vanshu <vanshugalhotra3332@gmail.com>
2022-03-28 18:17:43 +0530

    change the theme button: added

--- JS/logic.js
+++ JS/logic.js
@@ -48,6 +48,17 @@ function PageTransitions() {
         }
     });
 
+    // code for working the theme button
+    const themeBtn = document.querySelector('.theme-btn');
+
+    themeBtn.addEventListener('click', () => {
+
+        let element = document.body;
+        // toggle will add and remove the class whenever the button is clicked
+        element.classList.toggle('light-mode');
+
+    });
+
 }
 
 PageTransitions();
